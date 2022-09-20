30725b5c8ac0732a8bfa198b2859f2af49439d29
Mariela6 <99921480+Mariela6@users.noreply.github.com>
2022-09-20 18:50:22 -0300

    Nueva Carpeta FrontEnd
    Se movió a carpeta FrontEnd

---
+++ ProyectoFinal_UlloaMar/FrontEnd/src/app/components/hardys/hardys.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hardys',
+  templateUrl: './hardys.component.html',
+  styleUrls: ['./hardys.component.css']
+})
+export class HardysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
