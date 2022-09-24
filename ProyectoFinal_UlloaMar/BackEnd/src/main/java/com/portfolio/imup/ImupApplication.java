fd6be0cc29b4c2ddcc6123b66fcd202b35964e68
Mariela6 <99921480+Mariela6@users.noreply.github.com>
2022-09-23 22:12:19 -0300

    Actualizaciones
    Actualizaciones

---
+++ ProyectoFinal_UlloaMar/BackEnd/src/main/java/com/portfolio/imup/ImupApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.imup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImupApplication.class, args);
+	}
+
+}
---
+++ ProyectoFinal_UlloaMar/BackEnd/src/test/java/com/portfolio/imup/ImupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.imup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
